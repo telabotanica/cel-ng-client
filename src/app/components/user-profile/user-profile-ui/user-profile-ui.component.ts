d7b6927c30ee6e37ddce3dbf9c0c4ed7cc28c518
slack <yogi.margarine@yahoo.com>
2019-04-08 10:01:11 +0200

    User profile: added user information + a bit of cosmetic.

--- src/app/components/user-profile/user-profile-ui/user-profile-ui.component.ts
+++ src/app/components/user-profile/user-profile-ui/user-profile-ui.component.ts
@@ -1,5 +1,11 @@
+import { Inject } from '@angular/core';
 import { Component, OnInit } from '@angular/core';
 import { Router } from "@angular/router";
+import { environment } from '../../../../environments/environment';
+import { DOCUMENT } from '@angular/common';
+import * as jwt_decode from "jwt-decode";
+
+import { SsoService } from "../../../services/commons/sso.service";
 
 @Component({
   selector: 'app-user-profile-ui',
@@ -8,13 +14,46 @@ import { Router } from "@angular/router";
 })
 export class UserProfileUiComponent implements OnInit {
 
-  constructor(private router: Router) { }
+  private ssoAuthWidgetUrl = environment.sso.authWidgetUrl;
+  private decodedToken;
+
+  constructor(
+    private router: Router, 
+    private ssoService: SsoService,
+    @Inject(DOCUMENT) private document: any) { }
 
   ngOnInit() {
+    let token = this.ssoService.getToken();
+    this.decodedToken = this.getDecodedAccessToken(token);
+  }
+
+  getDecodedAccessToken(token: string): any {
+    try{
+        return jwt_decode(token);
+    }
+    catch(Error){
+        return null;
+    }
   }
 
   navigateToHelp() {
     this.router.navigate(['/help']);
+  }
+
+  getUsername() {
+    let pseudoUsed = this.decodedToken.pseudoUtilise;
+    if ( pseudoUsed ) {
+      return this.decodedToken.pseudo;
+    }
+    return this.decodedToken.prenom + ' ' + this.decodedToken.nom;
+  }
+
+  logout() {
+    this.document.location.href = this.ssoAuthWidgetUrl + '?action=deconnexion&origine=https://beta.tela-botanica.org/cel2-dev/cel2-client/dist/cel2-client';;
+  }
+
+  navigateToWpProfile() {
+    this.document.location.href = 'https://beta.tela-botanica.org/test/membres/'  + this.getUsername();
   }
 
 }
