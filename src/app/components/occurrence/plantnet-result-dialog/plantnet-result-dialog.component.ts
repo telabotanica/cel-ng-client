4a0b17e9c484a483a254833e42e7e6a80e842205
slack <yogi.margarine@yahoo.com>
2019-05-05 19:05:16 +0200

    BF: plantnet parsing PART 3.

---
+++ src/app/components/occurrence/plantnet-result-dialog/plantnet-result-dialog.component.ts
@@ -0,0 +1,41 @@
+import { Component, OnInit,EventEmitter } from '@angular/core';
+import {MAT_DIALOG_DATA} from '@angular/material';
+import { Inject } from '@angular/core';
+import { MatDialogRef } from "@angular/material";
+
+import { PlantnetResponse } from "../../../model/plantnet/plantnet-response.model";
+
+@Component({
+  selector: 'plantnet-result-dialog',
+  templateUrl: './plantnet-result-dialog.component.html',
+  styleUrls: ['./plantnet-result-dialog.component.css']
+})
+export class PlantnetResultDialogComponent implements OnInit {
+
+  constructor(
+    @Inject(MAT_DIALOG_DATA) public plantnetResponse: PlantnetResponse,
+    private dialogRef: MatDialogRef<PlantnetResultDialogComponent>) { }
+
+  ngOnInit() {
+
+  }
+
+    
+  close() {
+    this.dialogRef.close();
+  }
+
+  onResultSelect(sciName: string, authorship:string) {
+
+    let taxon = {
+        name: sciName,
+        repository: 'bdtfx', 
+        idNomen: null, 
+        author: authorship
+    }
+
+    this.dialogRef.close(taxon);
+  }
+
+
+}
