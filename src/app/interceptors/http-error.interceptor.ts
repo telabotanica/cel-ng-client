fa98942f35c0c4631f2f27dfe3ab5407ca2d8a1b
slack <yogi.margarine@yahoo.com>
2019-05-21 08:32:56 +0200

    Introduced WS error automatic notification using snackbar messages.

---
+++ src/app/interceptors/http-error.interceptor.ts
@@ -0,0 +1,47 @@
+import { Injectable } from '@angular/core';
+import {
+  HttpRequest,
+  HttpHandler,
+  HttpEvent,
+  HttpInterceptor,
+  HttpResponse,
+  HttpErrorResponse,
+} from '@angular/common/http';
+ 
+import { Observable } from 'rxjs/Observable';
+import { catchError } from 'rxjs/operators';
+import { throwError } from 'rxjs'
+import 'rxjs/add/operator/do';
+ 
+@Injectable()
+export class HttpErrorInterceptor implements HttpInterceptor {
+ 
+  constructor() {}
+ /*
+  intercept(request: HttpRequest<any>, next: HttpHandler): Observable<HttpEvent<any>> {
+
+
+
+    return next.handle(request).do((event: HttpEvent<any>) => {}, (err: any) => {
+      if (err instanceof HttpErrorResponse) {
+	// The SSO identitite service returns an HTTP error with status 400 
+ 	// when the user is not logged. This is not an error which should 
+	// be notified to the user but is part of the normal app flow.  
+        console.log("ERRRRRRRROR");
+      }
+    });
+*/
+
+    intercept(request: HttpRequest<any>, next: HttpHandler): Observable<HttpEvent<any>> {
+        return next.handle(request).pipe(catchError(err => {
+            if (err.status === 401) {
+                // auto logout if 401 response returned from api
+                //this.authenticationService.logout();
+                location.reload(true);
+            }
+
+            const error = err.error.message || err.statusText;
+            return throwError("error");
+        }))
+    }
+}
