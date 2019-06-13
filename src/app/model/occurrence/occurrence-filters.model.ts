fea3da5f794edf202fcdd762145da033b2021b7e
slack <yogi.margarine@yahoo.com>
2019-06-13 15:44:37 +0200

    BF: generated filter URL parameters was broken in the map.

--- src/app/model/occurrence/occurrence-filters.model.ts
+++ src/app/model/occurrence/occurrence-filters.model.ts
@@ -1,37 +1,47 @@
-export class OccurrenceFilters {
+export class OccurrenceFilters { //interface UrlParameterGenerator
 
-  dateObservedDay: number;
-  dateObservedMonth: number;
-  dateObservedYear: number;
-  family: string;
-  frenchDep: string;
-  userSciName: string;
-  locality: string;
-  signature:  string;
-  osmCountry: string;
-  isIdentiplanteValidated: boolean;
-  isPublic: boolean;
-  certainty: string;
-  projectId: number;
-  freeTextQuery: string;
-  tags: Array<string>;
-  ids: Array<string>;
-
-
-
-//interface UrlParameterGenerator
+  dateObservedDay:          number;
+  dateObservedMonth:        number;
+  dateObservedYear:         number;
+  family:                   string;
+  frenchDep:                string;
+  userSciName:              string;
+  locality:                 string;
+  signature:                string;
+  osmCountry:               string;
+  isIdentiplanteValidated:  boolean;
+  isPublic:                 boolean;
+  certainty:                string;
+  projectId:                number;
+  freeTextQuery:            string;
+  tags:                     Array<string>;
+  ids:                      Array<string>;
 
   toUrlParameters() : string  {
     let urlParams = '';
     for (var propertyName in this) {
-      if (this.hasOwnProperty(propertyName) && ! (this[propertyName] == null)) {
+
+      // Array type parameters
+      if (Array.isArray(this[propertyName])) {
+        for(var idx in this[propertyName]) {
+        urlParams += '&';
+        urlParams += propertyName;
+        urlParams += '[]=';
+        urlParams += this[propertyName][idx].toString();
+        }
+      }
+
+      // Simple type parameters
+      else if (this.hasOwnProperty(propertyName) && ! (this[propertyName] == null) ) {
         urlParams += '&';
         urlParams += propertyName;
         urlParams += '=';
         urlParams += this[propertyName].toString();
       }
+
     }
 
+    // Removes the first '&' and returns:
     return urlParams.substring(1);
   }
 
