d4348decdea09791411fd9e68e8c098a44930756
slack <yogi.margarine@yahoo.com>
2019-07-04 15:53:52 +0200

    Moved local var to .env

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -3,7 +3,7 @@
 // The list of file replacements can be found in `angular.json`.
 
 export const environment = {
-  production: false,
+  production: true,
   app : {
     title:           "Carnet en ligne V2",
     unsetTokenValue: "unset",
@@ -16,22 +16,18 @@ export const environment = {
     apiKey:        "843a36372facc0f1836f53d1d5968aa8",
     applicationId: "YOTVBFEBJC",
   },
+  plantnet: {
+    baseUrl: "https://beta.tela-botanica.org/cel2-dev/cel2-services/public/api/plantnet"
+  },
   api: {
     baseUrl: "https://beta.tela-botanica.org/cel2-dev/cel2-services/public/api",
     tagLibBaseUrl: "https://beta.tela-botanica.org/cel2-dev/cel2-services/public",
     prefix: "api",
   },
   telaWebSite: {
-    homepageUrl: 'https://www.tela-botanica.org',
     profileUrl: 'https://www.tela-botanica.org/membres/me/settings/profile',
+    homepageUrl: 'https://www.tela-botanica.org',
     contactUrl: 'https://www.tela-botanica.org/widget:reseau:remarques?lang=fr&service=cel&pageSource=https%3A%2F%2Fwww.tela-botanica.org%2Fcel%2Fappli%2Fcel2.html',
-  },
-  taxoApi: {
-    validationBaseUrl: "https://api.tela-botanica.org/service:eflore:0.1",
-    nameSearchBaseUrl: "https://api.tela-botanica.org/service:cel/NameSearch/"
-  },
-  plantnet: {
-    baseUrl: "https://beta.tela-botanica.org/cel2-dev/cel2-services/public/api/plantnet"
   },
   chorodep: {
     baseUrl: "https://api.tela-botanica.org/service:cel/InventoryTaxonPresent"
@@ -76,8 +72,75 @@ export const environment = {
       {path: 'Morphologie', name: 'Plantule', id: null, userId: null},
       {path: 'Morphologie', name: 'Rosette', id: null, userId: null},
     ]
+  },
+  tbTsbLib: {
+    // List of repositories for the taxon selection module:
+    tbRepositoriesConfig : [{
+        id: 'bdtfx',
+        label: 'Métropole',
+        levels: ['idiotaxon'],
+        apiUrl: 'https://api.tela-botanica.org/service:cel/NameSearch/bdtfx/',
+        apiUrl2: '',
+        apiUrlValidOccurence: 'https://api.tela-botanica.org/service:eflore:0.1/bdtfx/noms/',
+        description_fr: ''
+    }, {
+        id: 'bdtfxr',
+        label: 'Métropole (index réduit)',
+        levels: ['idiotaxon'],
+        apiUrl: 'https://api.tela-botanica.org/service:cel/NameSearch/bdtfxr/',
+        apiUrl2: '',
+        apiUrlValidOccurence: 'https://api.tela-botanica.org/service:eflore:0.1/bdtfxr/noms/',
+        description_fr: ''
+    }, {
+        id: 'bdtxa',
+        label: 'Antilles françaises',
+        levels: ['idiotaxon'],
+        apiUrl: 'https://api.tela-botanica.org/service:cel/NameSearch/bdtxa/',
+        apiUrl2: '',
+        apiUrlValidOccurence: 'https://api.tela-botanica.org/service:eflore:0.1/nva/noms/',
+        description_fr: ''
+    }, {
+        id: 'bdtre',
+        label: 'Réunion',
+        levels: ['idiotaxon'],
+        apiUrl: 'https://api.tela-botanica.org/service:cel/NameSearch/bdtre/',
+        apiUrl2: '',
+        apiUrlValidOccurence: 'https://api.tela-botanica.org/service:eflore:0.1/bdtre/noms/',
+        description_fr: ''
+    }, {
+        id: 'florical',
+        label: 'Nouvelle-Calédonie',
+        levels: ['idiotaxon'],
+        apiUrl: 'https://api.tela-botanica.org/service:cel/NameSearch/florical/',
+        apiUrl2: '',
+        apiUrlValidOccurence: 'https://api.tela-botanica.org/service:eflore:0.1/florical/noms/',
+        description_fr: ''
+    }, {
+        id: 'aublet',
+        label: 'Guyane',
+        levels: ['idiotaxon'],
+        apiUrl: 'https://api.tela-botanica.org/service:cel/NameSearch/aublet/',
+        apiUrl2: '',
+        apiUrlValidOccurence: 'https://api.tela-botanica.org/service:eflore:0.1/aublet/noms/',
+        description_fr: ''
+    }, {
+        id: 'apd',
+        label: 'Afrique',
+        levels: ['idiotaxon'],
+        apiUrl: 'https://api.tela-botanica.org/service:cel/NameSearch/apd/',
+        apiUrl2: '',
+        apiUrlValidOccurence: 'https://api.tela-botanica.org/service:eflore:0.1/apd/noms/',
+        description_fr: ''
+    }, {
+        id: 'lbf',
+        label: 'Liban',
+        levels: ['idiotaxon'],
+        apiUrl: 'https://api.tela-botanica.org/service:cel/NameSearch/lbf/',
+        apiUrl2: '',
+        apiUrlValidOccurence: 'https://api.tela-botanica.org/service:eflore:0.1/lbf/noms/',
+        description_fr: ''
+    }]
   }
-
 };
 
 /*
