680ed547f693c5bd2a8a9122e023fbecef2ecc29
slack <yogi.margarine@yahoo.com>
2019-07-09 17:19:05 +0200

    Added button to accept the Data usage agreement to the DUA component.

--- src/app/components/generic/user-agreement/user-agreement.component.ts
+++ src/app/components/generic/user-agreement/user-agreement.component.ts
@@ -1,4 +1,13 @@
 import { Component, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
+import { 
+  MatSnackBar
+} from "@angular/material";
+
+import { Profile 
+} from "../../../model/profile/profile.model";
+import { ProfileService } from "../../../services/profile/profile.service";
+import { TokenService } from "../../../services/commons/token.service";
 
 @Component({
   selector: 'app-user-agreement',
@@ -7,9 +16,37 @@ import { Component, OnInit } from '@angular/core';
 })
 export class UserAgreementComponent implements OnInit {
 
-  constructor() { }
+
+
+  constructor(
+    private _profileService: ProfileService,
+    private _snackBar: MatSnackBar,
+    private _router: Router,
+    private _tokenService: TokenService
+  ) { }
 
   ngOnInit() {
   }
 
+    acceptUserAgreement() {
+        let token = this._tokenService.getToken();
+        let userId = token.id;
+        let profile = new Profile();
+        profile.userId = userId;
+        this._profileService.post(profile).subscribe(
+            p => {
+                console.debug(p);
+          this._snackBar.open(
+          "Merci d'avoir accepté la charte d'utilisation.", 
+          "Fermer", 
+          { duration: 2500 });
+          this.navigateToOccurrenceGrid();
+
+            }
+        );
+
+    }
+    private navigateToOccurrenceGrid() {
+        this._router.navigateByUrl('/occurrence-ui');
+    }
 }
