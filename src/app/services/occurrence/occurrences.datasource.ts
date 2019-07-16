aa10df59018402da50213b8eba565422869ecaeb
slack <yogi.margarine@yahoo.com>
2019-07-16 08:43:32 +0200

    Beautified source some code.

--- src/app/services/occurrence/occurrences.datasource.ts
+++ src/app/services/occurrence/occurrences.datasource.ts
@@ -1,38 +1,71 @@
-import {Injectable} from "@angular/core";
-import { environment } from '../../../environments/environment';
-import {CollectionViewer, DataSource} from "@angular/cdk/collections";
-import {Observable} from "rxjs/Observable";
-import {HttpClient, HttpParams, HttpHeaders} from "@angular/common/http";
-import {BehaviorSubject} from "rxjs/BehaviorSubject";
-import {map, catchError, finalize} from "rxjs/operators";
-import {of} from "rxjs/observable/of";
-import 'rxjs/Rx' ;
+import {
+    Injectable
+} from "@angular/core";
+import {
+    environment
+} from '../../../environments/environment';
+import {
+    CollectionViewer,
+    DataSource
+} from "@angular/cdk/collections";
+import {
+    Observable
+} from "rxjs/Observable";
+import {
+    HttpClient,
+    HttpParams,
+    HttpHeaders
+} from "@angular/common/http";
+import {
+    BehaviorSubject
+} from "rxjs/BehaviorSubject";
+import {
+    map,
+    catchError,
+    finalize
+} from "rxjs/operators";
+import {
+    of
+} from "rxjs/observable/of";
+import 'rxjs/Rx';
 
-import { Occurrence } from "../../model/occurrence/occurrence.model";
-import { Photo } from "../../model/photo/photo.model";
-import { OccurrenceFilters } from "../../model/occurrence/occurrence-filters.model";
-import { JsonPatchService } from "../../../restit/services/json-patch.service";
-import { JsonPatchResponse } from '../../../restit/model/json-patch-response.model';
+import {
+    Occurrence
+} from "../../model/occurrence/occurrence.model";
+import {
+    Photo
+} from "../../model/photo/photo.model";
+import {
+    OccurrenceFilters
+} from "../../model/occurrence/occurrence-filters.model";
+import {
+    JsonPatchService
+} from "../../../restit/services/json-patch.service";
+import {
+    JsonPatchResponse
+} from '../../../restit/model/json-patch-response.model';
 
 //@todo refactor this ugly mess!
 @Injectable()
-export class OccurrencesDataSource implements DataSource<Occurrence> {
+export class OccurrencesDataSource implements DataSource < Occurrence > {
 
     // The array of Occurrence instances retrieved from the Web service:
-    public occurrencesSubject = new BehaviorSubject<Occurrence[]>([]);
-    private loadingSubject = new BehaviorSubject<boolean>(false);
+    public occurrencesSubject = new BehaviorSubject < Occurrence[] > ([]);
+    private loadingSubject = new BehaviorSubject < boolean > (false);
     public loading$ = this.loadingSubject.asObservable();
     private resourceUrl = environment.api.baseUrl + '/occurrences';
     private photoSubresourceSubpathEndpoint = '/photos';
 
-    constructor(private http:HttpClient, private jsonPatchService:JsonPatchService) {
+    constructor(private http: HttpClient, private jsonPatchService: JsonPatchService) {
 
     }
 
-    getPhotos(id) : Observable<Photo[]>{
+    getPhotos(id): Observable < Photo[] > {
         let endpoint = this.resourceUrl + '/' + id + this.photoSubresourceSubpathEndpoint;
-        return this.http.get<Photo[]>(endpoint, {
-            headers: {'Accept':'application/json'}
+        return this.http.get < Photo[] > (endpoint, {
+            headers: {
+                'Accept': 'application/json'
+            }
         });
     }
 
@@ -47,7 +80,7 @@ export class OccurrencesDataSource implements DataSource<Occurrence> {
     }
 
     findOccurrences(sortBy = '', sortDirection = 'asc',
-        pageNumber = 0, pageSize = 10, filters: OccurrenceFilters = null):  Observable<Occurrence[]> {
+        pageNumber = 0, pageSize = 10, filters: OccurrenceFilters = null): Observable < Occurrence[] > {
 
         let httpParams = this.buildParams(filters)
             .set('sortBy', sortBy)
@@ -55,7 +88,7 @@ export class OccurrencesDataSource implements DataSource<Occurrence> {
             .set('page', pageNumber.toString())
             .set('perPage', pageSize.toString());
 
-        return this.http.get<Occurrence[]>(this.resourceUrl + '.json', {
+        return this.http.get < Occurrence[] > (this.resourceUrl + '.json', {
             params: httpParams
         });
 
@@ -67,8 +100,8 @@ export class OccurrencesDataSource implements DataSource<Occurrence> {
     // We want ids[]=13&ids[]=15 instead so we use buildIdsUrlParams(ids).
     private buildIdsParams(ids) {
         let params = new HttpParams();
-        ids.forEach(function (id) {
-          params = params.append('id[]', id);
+        ids.forEach(function(id) {
+            params = params.append('id[]', id);
         });
         return params;
     }
@@ -77,98 +110,111 @@ export class OccurrencesDataSource implements DataSource<Occurrence> {
     //@todo merge with getCollection and add/use as a switch the Accept mimetype 
     generatePdfEtiquette(ids) {
         let params = this.buildIdsParams(ids);
-console.debug(params);
+        console.debug(params);
         return this.http.get(this.resourceUrl, {
             params: params,
             responseType: 'arraybuffer',
-            headers: {'Accept':'application/pdf'}
+            headers: {
+                'Accept': 'application/pdf'
+            }
         });
     }
 
 
-   generatePdfEtiquetteUrl(ids) {
+    generatePdfEtiquetteUrl(ids) {
         let params = this.buildIdsParams(ids);
         return this.resourceUrl + '/' + params.toString();
     }
 
-    get(id) : Observable<Occurrence>{
-        return this.http.get<Occurrence>(this.resourceUrl + '/' + id, {
-            headers: {'Accept':'application/json'}
+    get(id): Observable < Occurrence > {
+        return this.http.get < Occurrence > (this.resourceUrl + '/' + id, {
+            headers: {
+                'Accept': 'application/json'
+            }
         });
     }
 
     delete(id) {
-        return this.http.delete<Occurrence>(this.resourceUrl + '/' + id, {
-            headers: {'Accept':'application/json'}
+        return this.http.delete < Occurrence > (this.resourceUrl + '/' + id, {
+            headers: {
+                'Accept': 'application/json'
+            }
         });
     }
 
 
     patch(id, values) {
-        return this.http.patch<Occurrence>(this.resourceUrl + '/' + id, 
-        values,
-        {
-            headers: {'Accept':'application/json'}
-        });
+        return this.http.patch < Occurrence > (this.resourceUrl + '/' + id,
+            values, {
+                headers: {
+                    'Accept': 'application/json'
+                }
+            });
     }
 
     post(occurrence: Occurrence) {
-        return this.http.post<Occurrence>(
-            this.resourceUrl, 
-            occurrence,
-            {
-                headers: {'Accept':'application/json'}
+        return this.http.post < Occurrence > (
+            this.resourceUrl,
+            occurrence, {
+                headers: {
+                    'Accept': 'application/json'
+                }
             }
         );
     }
 
     importSpreadsheet(spreadsheetFile) {
-        let formData:FormData = new FormData();
+        let formData: FormData = new FormData();
         formData.append('file', spreadsheetFile, spreadsheetFile.name);
         let headers = {
             'Accept': 'application/json'
         };
 
-        return this.http.post(this.resourceUrl + '/import', formData, {headers: headers});
+        return this.http.post(this.resourceUrl + '/import', formData, {
+            headers: headers
+        });
 
     }
 
 
-    bulkRemove(ids): Observable<JsonPatchResponse[]> {
-        this.jsonPatchService.resourceServiceUrl = this.resourceUrl; 
+    bulkRemove(ids): Observable < JsonPatchResponse[] > {
+        this.jsonPatchService.resourceServiceUrl = this.resourceUrl;
+
         return this.jsonPatchService.bulkRemove(ids);
     }
 
-    bulkReplace(ids, value): Observable<JsonPatchResponse[]> {
-        this.jsonPatchService.resourceServiceUrl = this.resourceUrl; 
-         return this.jsonPatchService.bulkReplace(ids, value);
+    bulkReplace(ids, value): Observable < JsonPatchResponse[] > {
+        this.jsonPatchService.resourceServiceUrl = this.resourceUrl;
+
+        return this.jsonPatchService.bulkReplace(ids, value);
     }
 
-    bulkCopy(ids): Observable<JsonPatchResponse[]> {
-        this.jsonPatchService.resourceServiceUrl = this.resourceUrl; 
-         return this.jsonPatchService.bulkCopy(ids);
+    bulkCopy(ids): Observable < JsonPatchResponse[] > {
+        this.jsonPatchService.resourceServiceUrl = this.resourceUrl;
+
+        return this.jsonPatchService.bulkCopy(ids);
     }
 
-    loadOccurrences(sortProperty:string,
-                sortDirection:string,
-                pageIndex:number,
-                pageSize:number, 
-                filters: OccurrenceFilters=null) {
+    loadOccurrences(sortProperty: string,
+        sortDirection: string,
+        pageIndex: number,
+        pageSize: number,
+        filters: OccurrenceFilters = null) {
 
         this.loadingSubject.next(true);
 
         this.findOccurrences(sortProperty, sortDirection,
-            pageIndex, pageSize, filters).pipe(
-                catchError(() => of([])),
+                pageIndex, pageSize, filters).pipe(
+                catchError(() => of ([])),
                 finalize(() => this.loadingSubject.next(false))
             )
             .subscribe(occurrences => {
-              this.occurrencesSubject.next(occurrences);
+                this.occurrencesSubject.next(occurrences);
             });
 
     }
 
-    connect(collectionViewer: CollectionViewer): Observable<Occurrence[]> {
+    connect(collectionViewer: CollectionViewer): Observable < Occurrence[] > {
         console.log("Connecting data source");
         return this.occurrencesSubject.asObservable();
     }
@@ -178,36 +224,37 @@ console.debug(params);
         this.loadingSubject.complete();
     }
 
-    export(filters: OccurrenceFilters) {
+    export (filters: OccurrenceFilters) {
 
         let httpParams = this.buildParams(filters);
 
         return this.http.post(this.resourceUrl + '/export', '', {
             params: httpParams,
             responseType: 'arraybuffer',
-            headers: {'Accept':'text/csv'}
+            headers: {
+                'Accept': 'text/csv'
+            }
         });
     }
 
-   private buildParams(filters: OccurrenceFilters) {
-        let httpParams= new HttpParams();
+    private buildParams(filters: OccurrenceFilters) {
+        let httpParams = new HttpParams();
         console.debug(filters);
-        if ( filters !== null ) {
+        if (filters !== null) {
             for (var propertyName in filters) {
-                if (filters.hasOwnProperty(propertyName) && ! (filters[propertyName] == null)) {
+                if (filters.hasOwnProperty(propertyName) && !(filters[propertyName] == null)) {
                     if (Array.isArray(filters[propertyName])) {
                         for (var val in filters[propertyName]) {
                             httpParams = httpParams.append(propertyName + '[]', filters[propertyName][val]);
                         }
-                    }
-                    else {
+                    } else {
                         httpParams = httpParams.append(propertyName, filters[propertyName].toString());
                     }
                 }
             }
         }
-        return httpParams;
 
+        return httpParams;
     }
 
 }
--- src/app/services/photo/photo.service.ts
+++ src/app/services/photo/photo.service.ts
@@ -1,80 +1,96 @@
-import { Injectable } 
-  from '@angular/core';
-import { Observable } 
-  from "rxjs/Observable";
-import { HttpClient, HttpParams } 
-  from "@angular/common/http";
-import { environment } from '../../../environments/environment';
-import { Photo } 
-  from "../../model/photo/photo.model";
-import { PhotoFilters } 
-  from "../../model/photo/photo-filters.model";
-import { JsonPatchService } 
-  from "../../../restit/services/json-patch.service";
-import { JsonPatchResponse } 
-  from '../../../restit/model/json-patch-response.model';
+import {
+    Injectable
+}
+from '@angular/core';
+import {
+    Observable
+}
+from "rxjs/Observable";
+import {
+    HttpClient,
+    HttpParams
+}
+from "@angular/common/http";
+
+import {
+    environment
+} from '../../../environments/environment';
+import {
+    Photo
+}
+from "../../model/photo/photo.model";
+import {
+    PhotoFilters
+}
+from "../../model/photo/photo-filters.model";
+import {
+    JsonPatchService
+}
+from "../../../restit/services/json-patch.service";
+import {
+    JsonPatchResponse
+}
+from '../../../restit/model/json-patch-response.model';
 
 @Injectable({
-  providedIn: 'root'
+    providedIn: 'root'
 })
 export class PhotoService {
 
-  private resourceUrl = environment.api.baseUrl + '/photos';
+    private resourceUrl = environment.api.baseUrl + '/photos';
 
-  getCollection(
-    sortBy:string = "dateShot",
-    sortDirection:string = "desc",
-    pageNumber = 0, 
-    pageSize = 12, 
-    filters: PhotoFilters = null):  Observable<Photo[]> {
+    getCollection(
+        sortBy: string = "dateShot",
+        sortDirection: string = "desc",
+        pageNumber = 0,
+        pageSize = 12,
+        filters: PhotoFilters = null): Observable < Photo[] > {
 
-    let httpParams= new HttpParams()
+        let httpParams = new HttpParams()
             .set('page', pageNumber.toString())
             .set('perPage', pageSize.toString());
 
-    if ( sortBy !== null && sortBy !== "") {
-        httpParams = httpParams.append("sortBy", sortBy);
-    }
-    if ( sortDirection !== null && sortBy !== "") {
-        httpParams = httpParams.append("sortDirection", sortDirection);
-    }
-    if ( filters !== null ) {
-      
-      for (var propertyName in filters) {
-        if (filters.hasOwnProperty(propertyName) && ! (filters[propertyName] == null)) {
-
-          if (Array.isArray(filters[propertyName])) {
-              for (var val in filters[propertyName]) {
-                  httpParams = httpParams.append(propertyName + '[]', filters[propertyName][val]);
-              }
-          }
-          else {
-              httpParams = httpParams.append(propertyName, filters[propertyName].toString());
-          }
-
+        if (sortBy !== null && sortBy !== "") {
+            httpParams = httpParams.append("sortBy", sortBy);
         }
-      }
-    }
+        if (sortDirection !== null && sortBy !== "") {
+            httpParams = httpParams.append("sortDirection", sortDirection);
+        }
+        if (filters !== null) {
 
-    return this.http.get<Photo[]>(this.resourceUrl + ".json", {
-        params: httpParams
-    });
-
-  }
-
-    findCount(filters: PhotoFilters = null) {
-
-        let httpParams= new HttpParams();
-        console.debug(filters);
-        if ( filters !== null ) {
             for (var propertyName in filters) {
-                if (filters.hasOwnProperty(propertyName) && ! (filters[propertyName] == null)) {
+                if (filters.hasOwnProperty(propertyName) && !(filters[propertyName] == null)) {
+
                     if (Array.isArray(filters[propertyName])) {
                         for (var val in filters[propertyName]) {
                             httpParams = httpParams.append(propertyName + '[]', filters[propertyName][val]);
                         }
+                    } else {
+                        httpParams = httpParams.append(propertyName, filters[propertyName].toString());
                     }
-                    else {
+
+                }
+            }
+        }
+
+        return this.http.get < Photo[] > (this.resourceUrl + ".json", {
+            params: httpParams
+        });
+
+    }
+
+    findCount(filters: PhotoFilters = null) {
+
+        let httpParams = new HttpParams();
+        console.debug(filters);
+        if (filters !== null) {
+            for (var propertyName in filters) {
+                if (filters.hasOwnProperty(propertyName) && !(filters[propertyName] == null)) {
+                    if (Array.isArray(filters[propertyName])) {
+                        for (var val in filters[propertyName]) {
+                            httpParams = httpParams.append(propertyName + '[]', filters[propertyName][val]);
+                        }
+                    } else {
                         httpParams = httpParams.append(propertyName, filters[propertyName].toString());
                     }
                 }
@@ -87,56 +103,57 @@ export class PhotoService {
         });
     }
 
-  get(id) : Observable<Photo>{
-    return this.http.get<Photo>(this.resourceUrl + '/' + id, {
-      headers: {'Accept':'application/json'}
-    });
-  }
-
-  patch(id, values) : Observable<Photo>{
-    return this.http.patch<Photo>(
-      this.resourceUrl + '/' + id, 
-      values,
-      { headers: {'Accept':'application/json'} });
-  }
-
-  delete(id) {
-    return this.http.delete<Photo>(this.resourceUrl + '/' + id, {
-      headers: {'Accept':'application/json'}
-    });
-  }
-
-  download(ids) {
-
-    let httpParams= new HttpParams();
-
-    for (let id of ids) {
-      httpParams = httpParams.append('id[]', id);
-    } 
-
-    return this.http.get(this.resourceUrl + '/download', {
-      params: httpParams,
-      // this is what we really want but ng ony wants json or arraybuffer...
-      // responseType: 'application/zip' 
-      responseType: 'arraybuffer',
-    });
-  }
-
-  bulkReplace(ids, value): Observable<JsonPatchResponse[]> {
-    this.jsonPatchService.resourceServiceUrl = this.resourceUrl;
-    return this.jsonPatchService.bulkReplace(ids, value);
-  }
-
-  bulkRemove(ids): Observable<JsonPatchResponse[]> {
-    this.jsonPatchService.resourceServiceUrl = this.resourceUrl; 
-    return this.jsonPatchService.bulkRemove(ids);
-  }
-/*
-    bulkAssociateTags(ids): Observable<JsonPatchResponse[]> {
-        this.jsonPatchService.resourceServiceUrl = this.resourceUrl; 
-        return this.jsonPatchService.bulkRemove(ids);        
+    get(id): Observable < Photo > {
+        return this.http.get < Photo > (this.resourceUrl + '/' + id, {
+            headers: {
+                'Accept': 'application/json'
+            }
+        });
     }
-*/
 
-  constructor(private http:HttpClient, private jsonPatchService:JsonPatchService) { }
+    patch(id, values): Observable < Photo > {
+        return this.http.patch < Photo > (
+            this.resourceUrl + '/' + id,
+            values, {
+                headers: {
+                    'Accept': 'application/json'
+                }
+            });
+    }
+
+    delete(id) {
+        return this.http.delete < Photo > (this.resourceUrl + '/' + id, {
+            headers: {
+                'Accept': 'application/json'
+            }
+        });
+    }
+
+    download(ids) {
+
+        let httpParams = new HttpParams();
+
+        for (let id of ids) {
+            httpParams = httpParams.append('id[]', id);
+        }
+
+        return this.http.get(this.resourceUrl + '/download', {
+            params: httpParams,
+            // this is what we really want but ng ony wants json or arraybuffer...
+            // responseType: 'application/zip' 
+            responseType: 'arraybuffer',
+        });
+    }
+
+    bulkReplace(ids, value): Observable < JsonPatchResponse[] > {
+        this.jsonPatchService.resourceServiceUrl = this.resourceUrl;
+        return this.jsonPatchService.bulkReplace(ids, value);
+    }
+
+    bulkRemove(ids): Observable < JsonPatchResponse[] > {
+        this.jsonPatchService.resourceServiceUrl = this.resourceUrl;
+        return this.jsonPatchService.bulkRemove(ids);
+    }
+
+    constructor(private http: HttpClient, private jsonPatchService: JsonPatchService) {}
 }
